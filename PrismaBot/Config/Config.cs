b33cd9feb0d2ece98fb26ab7d59929b65c59b938
Rayth <Rayth@users.noreply.github.com>
2021-06-21 19:15:01 +0100

    Config Loader created
    Created class for creating and loading the config file.

---
+++ PrismaBot/Config/Config.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace PrismaBot.Config
+{
+    public class BotConfig
+    {
+        [JsonIgnore]
+        public static readonly string appdir = AppContext.BaseDirectory;
+
+        public string Prefix { get; set; }
+        public string DiscordToken { get; set; }
+        public string APIKey { get; set; }
+        public string APIUser { get; set; }
+        public string APIServer { get; set; }
+        public BotConfig()
+        {
+            Prefix = "";
+            DiscordToken = "";
+            APIKey = "";
+            APIUser = "";
+            APIServer = "";
+        }
+
+        public void Save(string dir = "Config.json")
+        {
+            string file = Path.Combine(appdir, dir);
+            File.WriteAllText(file, ToJson());
+        }
+        public static BotConfig Load(string dir = "Config.json")
+        {
+            string file = Path.Combine(appdir, dir);
+            return JsonConvert.DeserializeObject<BotConfig>(File.ReadAllText(file));
+        }
+        public string ToJson()
+            => JsonConvert.SerializeObject(this, Formatting.Indented);
+    }
+}
--- PrismaBot/Program.cs
+++ PrismaBot/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using PrismaBot.Config;
 
 namespace PrismaBot
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args) =>
+            new Program().Start().GetAwaiter().GetResult();
+
+        public async Task Start()
         {
-            
+            LoadConfig();
+        }
+
+        public static void LoadConfig()
+        {
+            string loc = $"{AppContext.BaseDirectory}Config.json";
+            //If File doesn't exist, create it. Ask user for inputs.
+            if (!File.Exists(loc))
+            {
+                var Config = new BotConfig(); //New config object
+                Console.WriteLine("This is the first time you have run this bot. Please enter some information to get set up.");
+                Console.Write("Discord Bot Token: "); Config.DiscordToken = Console.ReadLine();
+                Console.Write("Discord Command Prefix: "); Config.Prefix = Console.ReadLine();
+                Console.Write("Prisma API Key: "); Config.APIKey = Console.ReadLine();
+                Console.Write("Prisma API User: "); Config.APIUser = Console.ReadLine();
+                Console.Write("CubedHost Server ID: "); Config.APIServer = Console.ReadLine();
+                Config.Save();
+                Console.WriteLine("New Config file created, and loaded.");
+            } else
+            {
+                Console.WriteLine("Existing Config file located. Loaded.");
+            }
         }
     }
 }
